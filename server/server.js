e11eff74e198cf83b79f616167af9c3e6a417ded
Kelly Jackson <hi@kellyjackson.io>
2018-03-09 13:42:34 -0600

    Change GET /todos/:id error status

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
 const {User} = require('../models/user');
 const {Todo} = require('../models/todo');
 
@@ -27,6 +28,21 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos,
         code: 'ya ha ha'});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.send('Invalid todo ID');
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.send('This todo does not exist!');
+        }
+        res.send(todo.text);
     }, (err) => {
         res.status(400).send(err);
     });
